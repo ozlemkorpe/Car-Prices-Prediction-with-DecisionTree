5f8964dd3328e87aa3bacf15d1cc8285d869adc2
Ozlem <cse.ozlemkorpe@gmail.com>
2020-11-18 18:23:34 +0300

    Libraries fixed

--- CarPricesPredictor.py
+++ CarPricesPredictor.py
@@ -1,26 +1,16 @@
 #Import Libraries
-from pymongo import MongoClient
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import re
-from sklearn.model_selection import train_test_split, GridSearchCV,ShuffleSplit, cross_val_score
-from sklearn import model_selection
-from sklearn.neighbors import KNeighborsRegressor
-import statsmodels.api as sm
-from sklearn.linear_model import LinearRegression, Ridge, Lasso, LassoCV, ElasticNet
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-import numpy as np
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-import xgboost as xgb
-from xgboost import XGBRegressor
-import config
-import pickle
-from collections import defaultdict
-from operator import itemgetter
-import random
-import lightgbm as lgb
-from lightgbm import LGBMRegressor
+import seaborn as sns
+import scipy.stats as stats
+from scipy import stats
+from scipy.stats import norm, skew
 
 #Preprocessing
+
+#Read dataset
+data_train = pd.read_csv("train.csv")
+data_test = pd.read_csv("test.csv")
+
+data_train.head().T
